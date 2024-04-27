404672dce10cfc1ce0b08354b7c472e5d6271f62
Nicolas Aguirre <nicofrancis2002@gmail.com>
2024-04-27 11:26:44 +0300

    Delete and changes in search tree

--- Binary tree/10_1-4.cpp
+++ Binary tree/10_1-4.cpp
@@ -23,7 +23,6 @@ void f_print(Node* r, int indent = 1)
     f_print(r->left, indent + 3);
 }
 
-
 class Tree
 {
     Node *root=nullptr;
@@ -41,6 +40,12 @@ class Tree
             Node *new_n = new Node(key, data);
             this->insert_rec(new_n);
         }
+        cout << endl;
+    }
+
+    ~Tree()
+    {
+        n_des(root);
     }
 
     void insert_rec(Node *p)
@@ -106,6 +111,83 @@ class Tree
         return aux;
     }
 
+    void del_node_cycle(int key)
+    {
+        Node *aux = root;
+        Node *parent;
+        bool found = false;
+
+        while (aux != nullptr)
+        {
+            if (aux->key == key)
+            {
+                found = true;
+                break;
+            } else if (key < aux->key)
+            {
+                parent = aux;
+                aux = aux->left;
+            } else
+            {
+                parent = aux;
+                aux = aux->right;
+            }
+        }
+
+        if (!found)
+            cout << "Key not found" << endl;
+        
+        Node *last_parent;
+        Node *last = aux->right;
+        while (last->left != nullptr)
+        {
+            last_parent = last;
+            last = last->left;
+        }
+        
+        if (parent->left == aux)
+            parent->left = last;
+        else
+            parent->right = last;
+
+        if (last_parent->left == last)
+            last_parent->left = last->right;
+        else
+            last_parent->right = last->right;
+
+        last->right = aux->right;
+        last->left = aux->left;
+        delete aux;
+    }
+
+    Node *find_rec(int key)
+    {
+        return n_find(root, key);
+    }
+
+    void del_node_rec(int key)
+    {
+        Node *parent;
+        Node *aux = root;
+        n_del_find(parent, aux, key);
+        Node *last_parent;
+        Node *last = aux->right;
+        n_find_last(last, last_parent);
+        if (parent->left == aux)
+            parent->left = last;
+        else
+            parent->right = last;
+
+        if (last_parent->left == last)
+            last_parent->left = last->right;
+        else
+            last_parent->right = last->right;
+    
+        last->left = aux->left;
+        last->right = aux->right;
+        delete aux;
+    }
+
     void print()
     {
         f_print(root);
@@ -116,7 +198,51 @@ class Tree
     static int n_count(Node *p);
     static void n_insert(Node *p, Node *new_n);
     static Node *n_find(Node *p, int key);
+    static void n_del_find(Node *&parent, Node *&aux, int key);
+    static void n_find_last(Node *& last, Node *& last_parent);
+    static void n_des(Node *&n);
 };
+
+void Tree::n_des(Node *&n)
+{
+    if (n == nullptr)
+        return;
+    n_des(n->left);
+    n_des(n->right);
+    delete n;
+}
+
+void Tree::n_find_last(Node *& last, Node *& last_parent)
+{
+    if (last->left != nullptr)
+    {
+        last_parent = last;
+        last = last->left;
+        n_find_last(last, last_parent);
+    }
+}
+
+void Tree::n_del_find(Node *&parent, Node *&aux, int key)
+{
+    if (aux == nullptr)
+    {
+        cout << "Key not found" << endl;
+        exit(-1);
+    }
+
+    if (aux->key == key)
+        return;
+    else if (key < aux->key)
+    {
+        parent = aux;
+        aux = aux->left;
+    } else
+    {
+        parent = aux;
+        aux = aux->right;
+    }
+    n_del_find(parent, aux, key);
+}
 
 int Tree::n_count(Node *p)
 {
@@ -152,6 +278,11 @@ void Tree::n_insert(Node *p, Node *new_n)
 
 Node *Tree::n_find(Node *p, int key)
 {
+    if (p == nullptr)
+    {
+        cout << "Not found" << endl;
+        return nullptr;
+    }
     if (p->key == key)
         return p;
     else if (key < p->key)
@@ -166,10 +297,12 @@ int main ()
 
     G.print();
 
-    Node *found = G.find_cycle(10);
-    Node *found1 = G.find_cycle(5);
+    Node *found = G.find_rec(10);
+    Node *found1 = G.find_rec(5);
     cout << found->data << endl;
     cout << found1->data << endl;
+    G.del_node_cycle(3);
+    G.print();
 
     return EXIT_SUCCESS;
 }
